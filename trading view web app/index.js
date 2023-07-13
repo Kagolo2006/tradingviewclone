422f2a477574789bed59d647faef3de11666700f
Kagolo2006 <133035514+Kagolo2006@users.noreply.github.com>
2023-07-13 23:38:51 +0300

    Add files via upload

---
+++ trading view web app/index.js
@@ -0,0 +1,35 @@
+new TradingView.widget(
+    {
+        "autosize": true,
+        "symbol": "BINANCE:BTCUSDT",
+        "interval": "240",
+        "timezone": "Etc/Utc",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": true,
+        "withdateranges": true,
+        "hide_side_toolbar": false,
+        "allow_symbol_change": true,
+        "watchlist": [
+                "BINANCE:BTCUSDT",
+                "BINANCE:ETHUSDT",
+                
+        ],
+        "details": true,
+        "hotlist": true,
+        "calender": true,
+        "students": [
+            "STD;SMA"
+        ],
+        "container_id": "chart",
+        "show_popup_button": true,
+        "popup_width": "100",
+        "popup-height": "650"
+
+
+
+
+    }
+)
